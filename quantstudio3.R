54ba18031a423b8675dd9fbc84c499c475dcad06
Samuel Thompson <69192049+samt9892@users.noreply.github.com>
2022-04-26 13:13:53 +0800

    Add files via upload

--- quantstudio3.R
+++ quantstudio3.R
@@ -12,6 +12,7 @@ in.dir <- list.files(paste(wd), pattern = ".xls", full.names = TRUE)            
 #loop over all files
 for(i in seq_along(in.dir)) {
 pcr <-  read.xlsx(in.dir[i], sheetName = "Results")
+amp <- read.xlsx(in.dir[i], sheetName = "Amplification Data")
 
 #Clean pcr data
 colnames(pcr) <- pcr[42,]
@@ -19,9 +20,18 @@ pcr$filename.pcr <- pcr[[28, 2]]
 pcr$run_endtime.pcr <- pcr[[29, 2]]
 pcr <- pcr[-c(1:42), ]
 
+#Clean amp data
+colnames(amp) <- amp[42,]
+amp$filename.pcr <- amp[[28, 2]]
+amp$run_endtime.pcr <- amp[[29, 2]]
+amp <- amp[-c(1:43), ]
+amp <- subset(amp, Cycle == 50, select = c(`Well Position`, `Delta Rn`, `filename.pcr`, `run_endtime.pcr`))
+
+pcr <- inner_join(amp, pcr, by = c("Well Position", "filename.pcr", "run_endtime.pcr"))
+
 #Reorder columns
 pcr <- pcr %>% 
-  relocate(c("Sample Name", "CT", "Comments", "Tm1", "Tm2", "Tm3","Tm4"))
+  relocate(c("Sample Name", "CT", "Delta Rn", "Comments", "Tm1", "Tm2", "Tm3","Tm4"))
 
 #Output PCR file
 write.csv(pcr, paste(in.dir[i], "output.csv", sep = "_"))

9475106954a75ca0df22701537ec298b67bf61bf
Samuel Thompson <69192049+samt9892@users.noreply.github.com>
2022-04-27 11:44:18 +0800

    Added comments functionality
    added function to split comments into: 
    Annealing temperature (°C), primer concentration (uM), primer volume (uL), primer name, sample volume, sample name, Additional comments.

--- quantstudio3.R
+++ quantstudio3.R
@@ -1,9 +1,15 @@
-library(readxl)
-library(rJava) 
-library(xlsx) 
-library(dplyr)
-options(scipen=5)                                                               #reduce decimal places
+#Package names
+packages <- c("readxl", "rJava", "xlsx", "dplyr", "tidyr")
+# Install packages not yet installed
+installed_packages <- packages %in% rownames(installed.packages())
+if (any(installed_packages == FALSE)) {
+  install.packages(packages[!installed_packages])
+}
+# Packages loading
+invisible(lapply(packages, library, character.only = TRUE))
 
+
+options(scipen=5)                                                               #reduce decimal places
 wd <- dirname(rstudioapi::getSourceEditorContext()$path)                        #get current directory
 in.dir <- list.files(paste(wd), pattern = ".xls", full.names = TRUE)            #input file list
 
@@ -27,16 +33,26 @@ amp$run_endtime.pcr <- amp[[29, 2]]
 amp <- amp[-c(1:43), ]
 amp <- subset(amp, Cycle == 50, select = c(`Well Position`, `Delta Rn`, `filename.pcr`, `run_endtime.pcr`))
 
-pcr <- inner_join(amp, pcr, by = c("Well Position", "filename.pcr", "run_endtime.pcr"))
+pcr <- inner_join(amp, pcr, by = c("Well Position","filename.pcr","run_endtime.pcr"))
+
+#Split comments field
+pcr <- separate(pcr, "Comments", c("Annealing_temperature(°C)","Primer_concentration_(uM)","Primer_volume","Primer_name","Sample_volume","Additional_comments"),
+                 sep =",\\ ", remove = F, extra = "merge", fill = "warn")
 
 #Reorder columns
 pcr <- pcr %>% 
-  relocate(c("Sample Name", "CT", "Delta Rn", "Comments", "Tm1", "Tm2", "Tm3","Tm4"))
-}
+  relocate(c("Sample Name","CT","Delta Rn","Tm1","Tm2","Tm3","Tm4","Annealing_temperature(°C)","Primer_concentration_(uM)","Primer_volume","Primer_name","Sample_volume","Additional_comments"))
 
 #remove .xls suffix
-in.dir <- substr(in.dir,1,nchar(in.dir)-4) 
+in.dir[i] <- substr(in.dir[i],1,nchar(in.dir[i])-4) 
 #Output PCR file
-for(i in seq_along(in.dir)) {
-  write.csv(pcr, paste(in.dir[i], "output.csv", sep = "_"), row.names = FALSE)
+write.csv(pcr, paste(in.dir[i], "output.csv", sep = "_"), row.names = FALSE)
 }
+
+
+#remove .xls suffix
+#in.dir <- substr(in.dir,1,nchar(in.dir)-4) 
+#Output PCR file
+#for(i in seq_along(in.dir)) {
+  #write.csv(pcr, paste(in.dir[i], "output.csv", sep = "_"), row.names = FALSE)
+#}
